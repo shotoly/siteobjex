c0b7a7a0d1501ba5142446ae39201f97f73f2831
shotoly <114308804+shotoly@users.noreply.github.com>
2024-08-15 03:18:14 +0200

    Update and rename Contact.html to Contact.php

--- pages/Contact.php
+++ pages/Contact.php
@@ -40,6 +40,21 @@
 
             <button type="submit">Envoyer</button>
         </form>
+        if (isset($_POST['message'])) {
+            $entete  = 'MIME-Version: 1.0' . "\r\n";
+            $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+            $entete .= 'From: webmaster@monsite.fr' . "\r\n";
+            $entete .= 'Reply-to: ' . $_POST['email'];
+    
+            $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+            <p><b>Email : </b>' . $_POST['email'] . '<br>
+            <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
+    
+            $retour = mail('objex3d@gmail.Com', 'Envoi depuis page Contact', $message, $entete);
+            if($retour)
+                echo '<p>Votre message a bien été envoyé.</p>';
+        }
+        ?>
     </div>
     <footer>
         <p>&copy; 2024 Les pages de Robin. Tous droits réservés.</p>
