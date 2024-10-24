a0bd5a64e78adb04db937d92332a705a3ee49811
shotoly <114308804+shotoly@users.noreply.github.com>
2024-10-24 23:34:34 +0200

    Add files via upload

--- prog/menu.js
+++ prog/menu.js
@@ -1,6 +1,19 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');  // Ajoute ou enlève la classe active
+hamburger.addEventListener('click', (event) => {
+    menu.classList.toggle('active');  // Ajoute ou enlève la classe active sur le menu
+    
+});
+
+// Ferme le menu si on clique en dehors de celui-ci
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+
+    // Si le clic est en dehors du menu et du bouton hamburger
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+        menu.classList.remove('active'); // Ferme le menu
+        hamburger.classList.remove('active'); // Restaure l'icône hamburger
+    }
 });
